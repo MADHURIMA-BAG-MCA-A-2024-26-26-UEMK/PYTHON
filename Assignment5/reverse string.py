b2d434256ea34c39d59cf1a5e862331781593384
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-03-04 04:57:45 +0530

    04/03/25

---
+++ Assignment5/reverse string.py
@@ -0,0 +1,10 @@
+# Function to reverse a string
+def reverse_string(s):
+    return s[::-1]
+
+# Sample string
+string = "Hello, World!"
+
+# Calling the function and printing the reversed string
+reversed_str = reverse_string(string)
+print(f"The reversed string is: {reversed_str}")
